2058e6114342cecc4b0858f8300385a992ac4e9d
Kushagra Juneja <121512664+ku28@users.noreply.github.com>
2023-03-21 14:16:37 +0530

    Update PDFify.py

--- PDFify.py
+++ PDFify.py
@@ -1,6 +1,8 @@
 import os
 from time import strftime
 from sys import platform, exc_info
+import tkinter as tk
+from tkinter import filedialog, messagebox
 
 # Counts the number of files in the directory that can be converted
 def n_files(directory):
@@ -37,22 +39,7 @@ def is_tool(name):
 
     return which(name) is not None
 
-
-if __name__ == "__main__":
-    print('\nPlease note that this will overwrite any existing PDF files')
-    print('For best results, close Microsoft Word before proceeding')
-    input('Press enter to continue.')
-	
-    directory = os.getcwd()
-	
-    if n_files(directory) == 0:
-        print('There are no files to convert')
-        exit()
-		
-    createFolder(directory)
-	
-    print('Starting conversion... \n')
-
+def convert_files(directory):
     # Opens each file with Microsoft Word and saves as a PDF
     try:
         if(is_tool('libreoffice') == False):
@@ -84,4 +71,31 @@ if __name__ == "__main__":
     except Exception as e:
         print(e)
 
-    print('\nConversion finished at ' + strftime("%H:%M:%S"))
+def browse_directory():
+    global directory_label, directory_path
+    directory_path = filedialog.askdirectory()
+    directory_label.config(text=directory_path)
+
+def convert():
+    global directory_path
+    if n_files(directory_path) == 0:
+        messagebox.showwarning('No files', 'There are no files to convert')
+        return
+    createFolder(directory_path)
+    convert_files(directory_path)
+    messagebox.showinfo('Conversion complete', 'All files have been converted.')
+
+# Create GUI
+root = tk.Tk()
+root.title('PDFify')
+
+directory_label = tk.Label(root, text='Please select a directory')
+directory_label.pack()
+
+browse_button = tk.Button(root, text='Browse', command=browse_directory)
+browse_button.pack()
+
+convert_button = tk.Button(root, text='Convert', command=convert)
+convert_button.pack()
+
+root.mainloop()
